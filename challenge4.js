9699e70b5e1c15057fc28681322dbc5fd882d4bb
Jacob Mitchinson <jmitchinson+github@gmail.com>
2015-01-30 10:39:02 +0000

    completed challenge 4

--- challenge4.js
+++ challenge4.js
@@ -1,3 +1,33 @@
-var fs = require('fs');
+var fs = require('fs')
 
-var buf = fs.readFile(process.argv[2]);
+var lines = null
+
+function readContents(callback) {
+
+  fs.readFile(process.argv[2], function doneReading(err, fileContents) {
+
+    lines = fileContents.toString().split('\n').length - 1
+    callback()
+
+  })
+
+}
+
+function printLines() {
+
+  console.log(lines)
+
+}
+
+readContents(printLines)
+
+
+// Official solution
+// var fs = require('fs')
+//  var file = process.argv[2]
+
+//  fs.readFile(file, function (err, contents) {
+//    // fs.readFile(file, 'utf8', callback) can also be used
+//    var lines = contents.toString().split('\n').length - 1
+//    console.log(lines)
+//  })
